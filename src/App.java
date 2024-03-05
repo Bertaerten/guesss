8e3657f7f4774d37d03191bd617a32dd11faeb6a
Bert <Bertil.bjerring@gmail.com>
2024-03-05 08:38:03 +0100

    gyet

---
+++ src/App.java
@@ -0,0 +1,35 @@
+import processing.core.*;
+
+import classes.*;
+
+public class App  extends PApplet{
+
+    Hangman game;
+    public static void main(String[] args) {
+        String[] processingArgs = {"MySketch"};
+		App mySketch = new App();
+		PApplet.runSketch(processingArgs, mySketch);
+    }
+
+    public void settings(){
+        size(600, 400);
+    }
+
+    public void setup(){
+        game = new Hangman("Multiplication");
+    }
+
+    public void draw(){
+        background(0);
+        game.drawGuessedLetters(this);
+        if(!game.isAlive()){
+            background(125);
+        }
+    }
+
+    public void keyPressed(){
+        if (key != CODED){
+            game.guess(key);
+        }
+    }
+}
